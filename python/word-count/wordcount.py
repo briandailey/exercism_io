07394aa4f625ddba809afa8203fd495dfba022dd
Brian Dailey <brian@stratasan.com>
2014-11-21 22:27:30 -0600

    Initial import.

---
+++ python/word-count/wordcount.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+def word_count(phrase):
+    """ count occurances of each word in the phrase
+        and total them up. """
+
+    return dict(Counter(phrase.split()))
+
